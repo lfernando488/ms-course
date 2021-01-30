b1b01bd5a410d1e5819f7c9fa21deb9e06f2ab19
lfernando488 <lfernando488@gmail.com>
2021-01-30 06:45:48 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/ajaxdevhrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ajaxdevhrworker/resources/WorkerResource.java
@@ -36,6 +36,11 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		/*try {
+			Thread.sleep(3000L);
+		}catch (Exception e) {
+			
+		}*/
 		
 		logger.info("PORT= " + env.getProperty("local.server.port"));
 		
