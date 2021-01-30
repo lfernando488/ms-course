22b21e74d47dd0bc7feac4ecb1158ebe4206f374
lfernando488 <lfernando488@gmail.com>
2021-01-30 05:10:46 -0300

    Ribbon

--- hr-payroll/src/main/java/com/ajaxdev/hrpayroll/feignclients/WorkerClientFeign.java
+++ hr-payroll/src/main/java/com/ajaxdev/hrpayroll/feignclients/WorkerClientFeign.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.ajaxdev.hrpayroll.entities.Worker;
 
-
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path="/workers")
+@FeignClient(name = "hr-worker", path="/workers")
 public interface WorkerClientFeign {
 	
 	@GetMapping(value = "/{id}")
--- hr-worker/src/main/java/com/ajaxdevhrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ajaxdevhrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.ajaxdevhrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ajaxdevhrworker.entities.Worker;
 import com.ajaxdevhrworker.repositories.WorkerRepository;
 
+
 @RestController
 @RequestMapping(value = "/workers")
-public class WorkerResources {
+public class WorkerResource {
 
+	@Autowired
+	private Environment env;
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
 	@Autowired
 	private WorkerRepository workerRepository;
 	
@@ -27,6 +36,9 @@ public class WorkerResources {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		logger.info("PORT= " + env.getProperty("local.server.port"));
+		
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
